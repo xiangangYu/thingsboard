57a3b0a3a6c67ccad6f586611e3815a2f7f87926
yu-xian-gang <yu-xian-gang@163.com>
2024-02-18 11:50:04 +0800

    Actor增加注释

--- common/actor/src/main/java/org/thingsboard/server/actors/TbActor.java
+++ common/actor/src/main/java/org/thingsboard/server/actors/TbActor.java
@@ -20,6 +20,19 @@ import org.thingsboard.server.common.msg.TbActorStopReason;
 
 /**
  * Actor顶级接口
+ *
+ * Tb使用actor模型处理接收到的message，有效隔离了消息的接收和处理过程
+ *
+ * Actor模型三个重要概念
+ * 状态：Actor对象的变量信息，由Actor自己管理
+ * 行为：Actor中的计算逻辑，通过Actor接收到消息来改变Actor状态
+ * 邮箱：Actor和Actor之间的通信桥梁，邮箱内部通过FIFO消息队列来存储发送方Actor消息，接收方Actor从邮箱队列中获取消息
+ *
+ * TB中Actor与经典Actor模型对象关系
+ * TbActor  -->  Actor
+ * Dispatcher  -->  Behavior
+ * TbActorRef  -->  mailBox,TbActorRef对应的实现类为TbActorMailbox
+ *
  */
 public interface TbActor {
 
